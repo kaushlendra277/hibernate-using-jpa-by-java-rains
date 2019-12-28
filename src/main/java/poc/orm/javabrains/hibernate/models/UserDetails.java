f415bcfc86aa90d1af99d3c5ab3be301c4b719f4
kaushlendra277 <kaushlendra277@gmail.com>
2019-12-28 15:15:27 +0530

    added branch to use conter entity table to store relationship

--- src/main/java/poc/orm/javabrains/hibernate/models/UserDetails.java
+++ src/main/java/poc/orm/javabrains/hibernate/models/UserDetails.java
@@ -11,7 +11,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.Lob;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -61,12 +60,21 @@ public class UserDetails {
 			   // ****************
 			   // Bydefault it will create a new table whose default name OwnerEntityTableName_OwnedEntityVariableName(USERDETAILSENTITY_vvehicles)
 			   // this new table will have 2 columns , one for UserDetails PK and second for Vehicle PK
+			   // Use @JoinColumn to avoid this 3rd table as shown below
 			   // *************
 			   // To configure table name and columns name we use @JoinTable as below
+	/*
 	@JoinTable( // OPTIONAL
 			name = "user_vehicles", //  name of table
 			joinColumns = {@JoinColumn(name = "user_id")}, // this is used to update column associated with  UserDetails PK
 			inverseJoinColumns = {@JoinColumn (name = "vehicle_id")} // this is used to update column associated with  Vehicle PK
 			)
+			*/
+	// Alternative to @JoinTable is @JoinColumn
+	// We use @JoinColumn if we don't want 3rd table
+	@JoinColumn(
+			// IN CASE OF  @OneToMany  this will create a column in containedEntity table(Vehicle) 
+			// IN CASE OF  @ManyToOne  this will create a column in containerEntity table(USERDETAILSTABLE)	
+			name = "user_id") // 
 	private Collection<Vehicle> vvehicles;
 }
